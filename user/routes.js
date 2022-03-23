a2da1ab53c5d6305789eb835b5fbf54be880b1d6
SusanaChen <grupochen@hotmail.com>
2022-03-23 19:28:31 +0100

    rutas de get y post

---
+++ user/routes.js
@@ -0,0 +1,7 @@
+const sequelize = require ('sequelize');
+const express = require('express');
+const router = express.Router();
+const controller = require('./controller.js');
+
+router.get('/',controller.traerUsuarios);
+router.post('/',controller.crearUsuario);
