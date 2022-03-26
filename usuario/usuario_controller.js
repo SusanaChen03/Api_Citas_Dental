6fd44eb3d74b8149cbdfdd2d39ef94e499dac26e
SusanaChen <grupochen@hotmail.com>
2022-03-26 12:18:50 +0100

    arreglo path y poner try catch

--- usuario/usuario_controller.js
+++ usuario/usuario_controller.js
@@ -2,36 +2,47 @@ const Usuario = require("./usuario_model.js");
 const jwt = require("jsonwebtoken");
 
 
-
-module.exports.traerUsuario = async (req, res) => {
-  //GET DE Usuario //buscar por nombre del usuario
-  const lista = await Usuario.findAll({
-    where: {
-      nombre: req.query.nombre,
-    },
-  });
-  res.json(lista);
-};
-
-
-
-module.exports.crearUsuario = async (req, res) => {
-  //POST DE Usuario
-  const nuevoUsuario = {
-    nombre: req.body.nombre,
-    email: req.body.email,
-    contraseña: req.body.contraseña,
-    rol: req.body.rol,
+ //GET DE Usuario //buscar por nombre del usuario
+module.exports.traerUsuario = async (req, res) => {    
+  
+  try {
+    const lista = await Usuario.findAll({
+      where: {
+        nombre: req.query.nombre,
+      },
+    });
+    res.json(lista);
+  } catch (error){
+    res.json(error + "error")
   };
-
-  const usuarioCreado = await Usuario.create(nuevoUsuario);
-  res.status(201).json(usuarioCreado);
 };
+
+
+
+ //POST DE Usuario
+module.exports.crearUsuario = async (req, res) => {       
+
+  try{
+    const nuevoUsuario = {
+      nombre: req.body.nombre,
+      email: req.body.email,
+      contraseña: req.body.contraseña,
+      rol: req.body.rol,
+    };
+  
+    const usuarioCreado = await Usuario.create(nuevoUsuario);
+    res.status(201).json(usuarioCreado);
+  } catch (error) {
+    res.json(error + "error")
+  };
+};
+
 
 
 
 
 module.exports.login = async (req, res) => {
+
   try {
     const buscarUsuario = await Usuario.findOne({
       where: { email: req.body.email, contraseña: req.body.contraseña },
@@ -75,26 +86,32 @@ module.exports.logout = async (req,res)=> {
 
 // patch  no conseguido
 module.exports.editarUsuario = async(req,res)=>{
-
-    await Usuario.update({
-      id:req.body.did,
-    },{   
-      where: {
-        }
+  try{
+    await Usuario.update(req.body, {
+      where:{
+        id:req.params.id
+      }
     });
     res.json("Usuario editado");
-
+  } catch (error){
+    res.json(error + "error");
+  }
 };
 
 
-module.exports.borrarUsuario = async (req,res)=>{
 
-  await Usuario.destroy({
-    where: {
-      nombre:req.body.nombre
-    }
-  });
-  res.json("Usuario Borrado")
+//borrar usuario
+module.exports.borrarUsuario = async (req,res)=>{
+  try {
+    await Usuario.destroy({
+      where: {
+        id:req.body.id
+      }
+    });
+    res.json("Usuario Borrado")
+  } catch (error){
+    res.json(error + "error")
+  };
 };
 
 
@@ -103,7 +120,7 @@ module.exports.autorizacion = async (req,res)=> {
 
     try{
       const verificacion = jwt.verify(req.headers.token, process.env.JWT_SECRET);
-  if(verificacion == 'client' || verificacion =='admin'){
+  if(verificacion =='administrador'){
       next();
   }else{
           console.log('error else'+ verificacion);
@@ -112,5 +129,5 @@ module.exports.autorizacion = async (req,res)=> {
   }catch(e){
       console.log("el error es" + e)
   res.json (401);
-  }
-}
+  };
+};
--- usuario/usuario_routes.js
+++ usuario/usuario_routes.js
@@ -7,7 +7,7 @@ router.get("/", controller.traerUsuario);
 router.post("/", controller.crearUsuario);
 router.post("/login", controller.login);
 router.post("/logout",controller.logout);
-router.patch("/", controller.editarUsuario);
+router.patch("/:id", controller.editarUsuario);
 router.delete("/",controller.borrarUsuario);
 
 module.exports = router;
