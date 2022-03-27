45ea1981184a7c5b2504c7c1d6a956e799c28aef
David S <dvdsanar@gmail.com>
2022-03-27 16:03:21 +0200

    rutas pacientes

--- paciente/paciente_router.js
+++ paciente/paciente_router.js
@@ -2,10 +2,11 @@ const sequelize = require("sequelize");
 const express = require("express");
 const router = express.Router();
 const controller = require("./paciente_controller.js");
+const verificacion = require("../config/middlewares.js");
 
-router.get("/", controller.infoPaciente);
-router.post("/", controller.crearPaciente);
-router.patch("/:id", controller.modificarPaciente);
-router.delete("/", controller.bajaPaciente);
+router.get("/", verificacion(), controller.infoPaciente);
+router.post("/", verificacion(), controller.crearPaciente);
+router.patch("/:id", verificacion(), controller.modificarPaciente);
+router.delete("/", verificacion("admin"), controller.bajaPaciente);
 
 module.exports = router;
